c61f0ecdf00509d6b9a115a36bc023d7ba98750c
RenanKohler <renan.kohler@gmail.com>
2023-11-11 21:17:59 -0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,8 +14,8 @@ namespace ConsoleApp1
             var body = new ModifyAssistantRequest(); // ModifyAssistantRequest | 
             var assistantId = "asst_yMIlk3nq2bQ3Q2uO6Fe8XlnI";  // string | The ID of the assistant to modify.
 
-            //apiInstance.Configuration.AddApiKey("default", "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");
-            apiInstance.Configuration.AccessToken = "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";
+            
+            apiInstance.Configuration.AccessToken = "API_KEY";
             //apiInstance.AddDefaultHeader("Bearer", apiInstance.Configuration.ApiKey["default"]); ;
             try
             {                // Modifies an assistant.
